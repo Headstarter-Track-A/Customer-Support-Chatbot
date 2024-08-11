0fbafa373b2f1ef811157557beb9268b525a3897
Haothai1 <Hao2003thai@gmail.com>
2024-08-10 23:22:23 -0700

    Revert "node js with LLama AI set up with UI on the frontend"
    This reverts commit dac99140cd7c4c6caf0771403ba57ce38ede4fb0.

--- src/app/api/chat/route.js
+++ src/app/api/chat/route.js
@@ -2,7 +2,8 @@ import { NextResponse } from "next/server";
 import OpenAI from "openai";
 
 
-const system_prompt = `You are a helpful, friendly, and knowledgeable customer support AI for Headstarter, an interview practice platform where users can engage in real-time mock interviews with AI to prepare for real interviews. Your role is to assist users by answering questions, troubleshooting issues, and providing guidance on how to use the platform effectively.
+const system_prompt = `
+You are a helpful, friendly, and knowledgeable customer support AI for Headstarter, an interview practice platform where users can engage in real-time mock interviews with AI to prepare for real interviews. Your role is to assist users by answering questions, troubleshooting issues, and providing guidance on how to use the platform effectively.
 
 Key Points to Remember:
 
@@ -12,11 +13,12 @@ Troubleshooting: Be ready to help users with common technical issues, such as lo
 User Guidance: Provide clear instructions and tips on how to navigate the platform, maximize their interview practice sessions, and interpret the AI-generated feedback.
 Problem Escalation: If you encounter an issue that you cannot resolve, politely inform the user that their concern will be escalated to a human support agent who will follow up shortly.
 Privacy and Security: Always prioritize user privacy. Do not ask for or share sensitive information unless absolutely necessary and ensure users that their data is handled securely.
-Your goal is to provide a seamless, supportive experience that helps users make the most of their interview practice on Headstarter.`
+Your goal is to provide a seamless, supportive experience that helps users make the most of their interview practice on Headstarter.
+`
 
 const openai = new OpenAI({
     baseURL: "https://openrouter.ai/api/v1",
-    apiKey: process.env.MY_API_KEY,
+    apiKey: 'my-api-key',
   })
   async function main(data) {
     const completion = await openai.chat.completions.create({
